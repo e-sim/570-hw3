4aa2ed1e207d1c96110dbf7afba4656ed30f9171
Erica <ejsim@uwalumni.com>
2017-10-19 18:46:26 -0500

    works for wfst1

--- fst_acceptor2.py
+++ fst_acceptor2.py
@@ -27,8 +27,8 @@ class Node:
 
     #for testing
     def __repr__(self):
-        return "name=" + self.name + " accept=" + str(self.accept)\
-                + " (" + str(self.adjacents) + ")"
+        return "name=" + self.name + " accept=" + str(self.accept)
+               # + " (" + str(self.adjacents) + ")"
 
     def set_accept(self):
         self.accept = True
@@ -36,19 +36,37 @@ class Node:
 class TrelSquare:
 
     def __init__(self, prob, prev_state, out_str, state_node, step):
+        debug_print(str(prob) + " " + str(state_node.name) if state_node else "Nothing")
         self.prob = prob
         self.prev_state = prev_state
         self.out_str = out_str
         self.state = state_node
         self.step = step
 
+    def __repr__(self):
+        state_name = "None" 
+        prev_state = "None"       
+        if self.state:
+            state_name = self.state.name
+
+        if self.prev_state:
+            prev_state = self.prev_state.name
+        
+        return "|  SQUARE prob= " + str(self.prob) + " on step " + str(self.step) + " ||"
+
     def find_prob(self, dest_node, curr_word):
         #the probability of going to the destination node/state from this one
         prob = 0
         new_out = ""
+        if self.prob == 0 or not self.state:
+            return (0, "")
+        
         for edge in self.state.adjacents:
-            if edge.dest is dest_node.name and edge.in_word is curr_word:
-                new_prob = self.prob * edge.weight
+            debug_print("\t\t\tedge word: " + edge.in_word + " current word: " + curr_word)
+            debug_print("\t\t\tedge dest: " + edge.dest + " dest node: " + dest_node.name)
+            if edge.dest == dest_node.name and edge.in_word == curr_word:
+                new_prob = self.prob * float(edge.weight)
+                debug_print("\t\t\tprob: " + str(self.prob) + " edge weight: " + str(edge.weight))
                 if new_prob > prob:
                     new_out = edge.output
                     prob = new_prob
@@ -58,17 +76,28 @@ class TrelSquare:
 
 def find_max_prob(column, dest_node, curr_word):
     max_prob = 0
+    max_owner = None
+    output = ""
     for origin_sq in column:
-        (curr_prob, new_out) = origin_sq.find_prob(dest_node, curr_word)
+        debug_print("\torigin sq is {0}".format(str(origin_sq)))
+        prob_tup = origin_sq.find_prob(dest_node, curr_word)
+        curr_prob = prob_tup[0]
+        new_out = prob_tup[1]
+
+        debug_print("\t\t" + str(prob_tup))
         if curr_prob > max_prob:
             max_prob = curr_prob
             max_owner = origin_sq.state
-            output = origin_sq.out_str + new_out
+            if origin_sq.out_str: 
+                output = origin_sq.out_str + " \"" + new_out + '"'
+            else:
+                output = '"' + new_out + '"'
 
     return (max_prob, max_owner, output)
 
 def find_final_prob(final_column):
     final_prob = 0
+    output = ""
     for square in final_column:
         if square.prob > final_prob and square.state.accept:
             final_prob = square.prob
@@ -78,7 +107,7 @@ def find_final_prob(final_column):
 
 
 #### debug print
-DEBUGGING = False
+DEBUGGING = True
 def debug_print(text):
     if DEBUGGING:
         print(text)
@@ -93,17 +122,21 @@ FST_FILE = open(sys.argv[1], "r")
 node_dict = collections.OrderedDict()
 
 for line in FST_FILE:
-
+    line = line.rstrip()
     #regex for each line of fst file
     FST_ACCEPT_REGEX_PATTERN = re.compile(r"(\w+)$")
-    FST_EDGE_REGEX_PATTERN = re.compile(r"\((\w+) \((\w+) \"(\w*)\" \"(\w*)\" (\d(\.\d+)?)\)\)")
-    accept_match = FST_ACCEPT_REGEX_PATTERN.match(line.strip())
+    FST_EDGE_REGEX_PATTERN = re.compile(r"\((\w+) \((\w+) \"(\w*)\" +\"(\w*)\" (\d(\.\d+))?\)\)")
+    accept_match = FST_ACCEPT_REGEX_PATTERN.match(line)
 
     if accept_match:
         acc_state = accept_match.group(1)
 
     else:
         edge_match = FST_EDGE_REGEX_PATTERN.match(line)
+
+        if not edge_match:
+            raise Exception("'{0}' is not a valid edge string".format(line))
+
         curr = edge_match.group(1)
         next_node_code = edge_match.group(2)
         in_word = edge_match.group(3)
@@ -118,16 +151,15 @@ for line in FST_FILE:
 
             new_node = Node(curr)
             node_dict[curr] = new_node
-            debug_print("made " + str(new_node))
-
+            
             if acc_state is curr:
                 new_node.set_accept()
+
+            debug_print("made " + str(new_node))
 
 
         new_edge = Edge(in_word, next_node_code, weight, out_word)
         node_dict[curr].adjacents.append(new_edge)
-
-        debug_print("adjacents are  " + str(node_dict[curr].adjacents))
 
 
 FST_FILE.close()
@@ -140,20 +172,21 @@ INFILE = open(sys.argv[2], "r")
 for line in INFILE:
 
     orig_line = line.strip("\n")
-    line = line.translate(None, '" ')
+    line = line.translate(None, '"')
     inputs = line.split()
     dummy = Node("start")
-    trellis = [[TrelSquare(0, dummy, "", None, -1) for x in range(len(inputs))] for i in node_dict.keys()]
+    debug_print("length of dictionary: " + str(len(node_dict)))
+    trellis = [[TrelSquare(0, dummy, "", None, -1) for x in range(len(inputs) + 1)] for i in range(len(node_dict))]
 
     #probably also need to adjust format of output (should it be a list?)
 
-    curr_node = node_dict.itervalues().next()
+    curr_node = node_dict.values()[0]
     curr_word = inputs[0]
     out_str = ""
     step = 0
 
     # first one:
-    trellis[0][0] = TrelSquare(1, dummy, "", curr_node, step)
+    trellis[0][step] = TrelSquare(1.0, dummy, "", curr_node, step)
     step += 1
 
     #trelsquare has prob, prevnode, outstr, destnode, step
@@ -162,37 +195,42 @@ for line in INFILE:
 
     # prob = self.find_prob[0]
     # newout = self.find_prob[1]
+    debug_print("inputs are: " + str(inputs).translate(None, ))
+    debug_print("length of input: " + str(len(inputs)))
 
     #loop through steps/chars of line/columns in trellis (all ~equiv)
-    while step < len(inputs):
-
-        curr_word = inputs[step-1]
+    for curr_word in inputs:
+        debug_print("step is: " + str(step))
+        #curr_word = inputs[step-1]
         #loop through squares in curr column & fill out
         #list = [array[step] for array in trellis]
         #for square in list:
         j = 0
         while j < len(trellis):
-            curr_node = node_dict.items()[j]
+            curr_node = node_dict.values()[j]
+            debug_print("dictionary: " + str(node_dict))
 
             #loop through possible origin squares (prev column) to find max prob
-            prev_col = [array[step-1] for array in trellis]
-            #for origin_sq in prev_col:
-            #    (curr_prob, out) = origin_sq.find_prob(curr_node, curr_char)
-            #    if curr_prob > max_prob:
-            #        max_prob = curr_prob
-            #        max_owner = origin_sq.state
-            #        old_out = origin_sq.out_str
-            #        new_out = out
-            (max_prob, max_owner, out_so_far) = find_max_prob(prev_col, curr_node,
-                                                              curr_word)
+            #prev_col = [x[step-1] for x in trellis]
+            prev_col = []
+            for x in trellis:
+                prev_col.append(x[step-1])
+           # debug_print("prev column: {0}".format(prev_col))
+            max_tup = find_max_prob(prev_col, curr_node, curr_word)
+            debug_print(max_tup)
+            max_prob = max_tup[0]
+            max_owner = max_tup[1]
+            out_so_far = max_tup[2]
 
             trellis[j][step] = TrelSquare(max_prob, max_owner, out_so_far,
                                           curr_node, step)
+            #debug_print("TRELLIS: " + str(trellis))
             j += 1
 
         step += 1
 
-    last_col = [array[len(inputs)-1] for array in trellis]
+    #debug_print(trellis)
+    last_col = [array[len(inputs)] for array in trellis]
     (final_prob, output) = find_final_prob(last_col)
 
     print(orig_line + " => " + output + " " + str(final_prob))
